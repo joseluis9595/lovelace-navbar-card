dc89cf15da4f48b7c3e0322094101d5261610604
Jose Luis Álvarez <joseluisalvquiroga@gmail.com>
2026-02-17 11:28:15 +0100

    Release/1.4.0 (#276)
    * feat: replace editor URL text input with HA native URL picker (#248)
    
    * fix: properly read default values for haptic configuration (#247)
    
    * Feature/desktop mode (#246)
    
    * feat: biome
    
    * fix: docs links on console logs and helpers
    
    * feat: new `mode` option for desktop configuration
    
    * fix: biome configuration
    
    * chore: revert biome configuration
    
    * Chore/biome (#254)
    
    * feat: added biome configuration
    
    * chore: biome lint fixes
    
    * chore: linter errors + typescript checking when building
    
    * chore: remove all references to eslint
    
    * chore: update `@types/bun`
    
    * Hotfix/editor url picker (#257)
    
    * chore: bump version
    
    * fix: ha-navigation-picker not being loaded to HA when using navbar editor
    
    * Feature/media player desktop (#256)
    
    * chore: added biome as linter
    
    * chore: replace linter in package.json scripts
    
    * feat: media player desktop mode
    
    * chore: updated docs
    
    * docs: refactored examples grid & added examples for media player configuration
    
    * docs: update `show` property on desktop media player example
    
    * chore: biome-ignore
    
    * Feature/media player icon and title (#274)
    
    * feat: render fallback html select when ha-combo-box is not found in the customElements registry
    
    * feat: allow empty values for native HTML dropdown inputs
    
    * chore: split editor style files
    
    * chore: remove unused comments
    
    * chore: lint
    
    * feat: add new `title`, `subtitle` and `icon` options for the media player widget
    
    * Fix/missing ha combo box (#272)
    
    * feat: render fallback html select when ha-combo-box is not found in the customElements registry
    
    * feat: allow empty values for native HTML dropdown inputs
    
    * chore: split editor style files
    
    * chore: remove unused comments
    
    * chore: lint
    
    * feat: add support for relative URLs (#275)
    
    * chore: version bump
    
    * chore: bump dependencies
    
    * Feature/quickbar with no filters (#277)
    
    * chore: version bump
    
    * feat: allow quickbar to be opened with no custom filter applied
    
    * docs: reference Quick search documentation in "Custom actions" docs
    
    * chore: update "stale" workflow to only target issues with "more info needed" label
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './base-types/object';
 export * from './base-types/string';
+export * from './docs-links';
 export * from './dom';
 export * from './haptic';
 export * from './template';
